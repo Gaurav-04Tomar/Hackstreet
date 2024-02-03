54006ea6ce233cbd0fcc6c7de5f7ed0a6a60c765
Gaurav Tomar <gaurav04jaat@gmail.com>
2024-02-03 18:46:50 +0530

    added frontend

---
+++ frontend/src/components/Lund.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import TextInput from "./TextInput";
+import CustomButton from "./CustomButton";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width:700,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const Lund = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Message</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+        <form
+        className='hidden md:flex items-center justify-center m-10'
+      >
+        <TextInput
+          placeholder='Send message...'
+          styles='w-[18rem] lg:w-[38rem]  rounded-l-full py-3 '
+        />
+        <CustomButton
+          title='send'
+          type='submit'
+          containerStyles='bg-[#0444a4] text-white px-6 py-2.5 mt-2 rounded-r-full'
+        />
+      </form>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default Lund;
